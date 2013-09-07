09a2d3fcac6e6f1e74967c3de80d5e04cef56f9b
johnny <johnny2k@gmail.com>
2013-09-07 05:49:51 -0700

    Adds AddClip method that is responsible for adding buttons

--- rsb.py
+++ rsb.py
@@ -12,7 +12,8 @@ class RyoubaBoard(wx.Frame):
 
         filemenu = wx.Menu()
         menuAbout = filemenu.Append(wx.ID_ABOUT, "&About","Push buttons to make Ryouba talk!")
-        menuOpen = filemenu.Append(wx.ID_OPEN,"&Open","Select a clip.")
+        menuAdd = filemenu.Append(wx.ID_ADD,"&Add Sound","Select a clip.")
+        menuOpen= filemenu.Append(wx.ID_OPEN, "&Open", "Open a soundboard")
         menuSaveAs = filemenu.Append(wx.ID_SAVEAS, "Save &As...", "Save this shit")
         menuExit = filemenu.Append(wx.ID_EXIT,"&Exit","Run away!")
 
@@ -23,7 +24,7 @@ class RyoubaBoard(wx.Frame):
 
         self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
-        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+        self.Bind(wx.EVT_MENU, self.OnAdd, menuAdd)
         self.Bind(wx.EVT_MENU, self.OnSaveAs, menuSaveAs)
 
         self.buttons = []
@@ -37,11 +38,6 @@ class RyoubaBoard(wx.Frame):
         #self.AutoLayout =(1)
         #self.sizer.Fit(self)
         self.Show()
-
-  # variables needed a long the way. this probably bad
-
-#    def AddButton(self, filename):
-# RyoubaBoard.sizer2.Add(RyoubaBoard.buttons.append(wx.Button(self, -1, "Button &"+str(filename)))) 
     
     def OnAbout(self, e):
         dlg = wx.MessageDialog(self, "Push buttons to make Ryouba talk!", "About Ryouba Sound Board", wx.OK)
@@ -69,9 +65,9 @@ class RyoubaBoard(wx.Frame):
            file.close()
        dlg.Destroy() 
 
-    def OnOpen(self,e):
+    def OnAdd(self,e):
       global buttoncount
-      """Open a clip"""
+      """Add a clip"""
       self.dirname = "clips" if self.directory is None else self.directory
 
       dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
@@ -81,18 +77,18 @@ class RyoubaBoard(wx.Frame):
           self.dirname = dlg.GetDirectory()
           self.directory = self.dirname
           file = open(os.path.join(self.dirname, self.filename), 'r')
-          
-          # The button creation and binding should be a separate function. 
-          self.buttons.append(wx.Button(self, label=string.capitalize(self.filename[:-4])))
+          self.AddClip(self.dirname, self.filename)
+          file.close()
+      dlg.Destroy()
+
+    def AddClip(self, path, name):
+          self.buttons.append(wx.Button(self, label=string.capitalize(name[:-4])))
           #i = len(self.buttons) - 1
           #self.Bind(wx.EVT_BUTTON, self.OnClick(file), self.buttons[i])
           self.buttons[-1].Bind(wx.EVT_BUTTON, self.OnClick)
-          self.buttons[-1].myname = os.path.join(self.dirname, self.filename)
+          self.buttons[-1].myname = os.path.join(path, name)
           self.sizer.Add(self.buttons[-1], 0, wx.ALIGN_CENTER|wx.ALL, 5)
           self.Fit()
-
-          file.close()
-      dlg.Destroy()
 
     def OnClick(self, event):
           audio.sayit(event.GetEventObject().myname)
